d3042ce7333eb34f9f83b565bd50c106b2629a9f
Brandon Smylie <bsmylie873@gmail.com>
2025-02-12 12:10:47 -0500

    Removed unused util.

--- csharp/src/api/Program.cs
+++ csharp/src/api/Program.cs
@@ -1,11 +1,9 @@
-using api.Utils;
 using services.Interfaces;
 using services.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IGameService, GameService>();
-builder.Services.AddTransient<IIdentifierGenerator, IdentifierGenerator>();
 builder.Services.AddAutoMapper(config => config.AllowNullCollections = true, typeof(Program).Assembly,
     typeof(GameService).Assembly);
 
